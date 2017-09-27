2464b87b77b439e9c12b253267586b3c5c80f8d3
Shreya Bajpai <sb1437@scarletmail.rutgers.edu>
2017-09-26 22:35:22 -0400

    Update sorter.c

--- sorter.c
+++ sorter.c
@@ -9,8 +9,4 @@ typedef struct movies[5044]; //Number of rows in .csv file
 int main (int argc, char * argv[]) { 
 	FILE * fp; //opens .csv to be read
 	fp = stdin; 
-
-	if (argc != 3 || strcmp(argv[1],"-c") != 0) {
-        fprintf("Your argument must be in the following format: cat input.file | ./sorter -c  movie_title > output.file");
-    } 
 }
